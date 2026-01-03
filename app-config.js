2374f043ccdcaec6d0ff5f4cba45a91bdfe1aefe
mcincura <mar.cincura@gmail.com>
2026-01-03 13:41:45 +0100

    Updates

---
+++ app-config.js
@@ -0,0 +1,10 @@
+// Verejná (netajná) konfigurácia frontendu.
+// Tento súbor sa načítava priamo a môžeš ho upraviť bez rekompilácie.
+//
+// DÔLEŽITÉ: Nezadávaj sem žiadne tajomstvá.
+window.__APP_CONFIG__ = {
+  // Príklad:
+  // API_BASE_URL: 'https://api.tvojadomena.sk'
+  // Tip pre vývoj: nechaj prázdne pre použitie Vite proxy (`/api`).
+  API_BASE_URL: 'https://zajky.zentrobot.io'
+};
